12e03bfeb5eaad9c7025517abaac46beed1b9ff3
David Lev <davidlev050414@gmail.com>
2023-05-14 00:26:30 +0300

    [tg] adding handlers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.handlers import InlineQueryHandler, MessageHandler, CallbackQueryHandler
-
 from tg import search
 from tg import browse
 from tg import utils
@@ -19,7 +18,7 @@ app.add_handler(
 )
 
 app.add_handler(
-    InlineQueryHandler(search.search_books, filters=filters.create(lambda _, __, query: len(query.query) > 2)),
+    InlineQueryHandler(search.search_books_inline, filters=filters.create(lambda _, __, query: len(query.query) > 2)),
 )
 app.add_handler(
     InlineQueryHandler(search.empty_search, filters=filters.create(lambda _, __, query: len(query.query) <= 2))
@@ -33,13 +32,54 @@ app.add_handler(
 
 app.add_handler(
     CallbackQueryHandler(
-        browse.browse, filters=filters.create(lambda _, __, query: len(query.data.split(":")) == 2)
+        browse.browse, filters=filters.create(
+            lambda _, __, query: query.data.startswith("browse") and len(query.data.split(":")) == 2
+        )
     )
 )
 
 app.add_handler(
     CallbackQueryHandler(
-        browse.browse_results, filters=filters.create(lambda _, __, query: len(query.data.split(":")) == 5)
+        browse.browse_results, filters=filters.create(
+            lambda _, __, query: query.data.startswith("browse") and len(query.data.split(":")) == 5
+        )
+    )
+)
+
+app.add_handler(
+    MessageHandler(
+        search.search_books_message, filters=(
+                filters.text & ~filters.via_bot & filters.create(lambda _, __, msg: len(msg.text) > 2))
+    )
+)
+
+app.add_handler(
+    CallbackQueryHandler(
+        search.search_books_callback, filters=filters.create(
+            lambda _, __, query: query.data.startswith("search") and len(query.data.split(":")) == 3
+        )
+    )
+)
+
+app.add_handler(
+    CallbackQueryHandler(
+        utils.read, filters=filters.create(lambda _, __, query: query.data.startswith("read"))
+    )
+)
+
+app.add_handler(
+    CallbackQueryHandler(
+        browse.browse_book, filters=filters.create(
+            lambda _, __, query: query.data.startswith("browse:book")
+        )
+    )
+)
+
+app.add_handler(
+    CallbackQueryHandler(
+        search.search_book, filters=filters.create(
+            lambda _, __, query: query.data.startswith("search:book")
+        )
     )
 )
 

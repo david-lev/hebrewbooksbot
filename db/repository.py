5e0cf1ea4d4219c0c7999e5df2ea766fc3335448
David Lev <davidlev050414@gmail.com>
2023-05-21 12:23:15 +0300

    [db,repository] dynamic updates

--- db/repository.py
+++ db/repository.py
@@ -19,38 +19,44 @@ def get_tg_user(tg_id: int) -> type[TgUser]:
     return session.query(TgUser).filter(TgUser.tg_id == tg_id).first()
 
 
-def get_tg_users_count() -> int:
+def get_tg_users_count(active: bool | None = None, lang_code: str = None) -> int:
     """Get tg users count"""
     session = get_session()
-    return session.query(TgUser).count()
+    tg_users = session.query(TgUser)
+    if active is not None:
+        tg_users = tg_users.filter(TgUser.active == active)
+    if lang_code:
+        tg_users = tg_users.filter(TgUser.lang == lang_code)
+    return tg_users.count()
 
 
 def get_tg_user_lang(tg_id: int) -> str | None:
     """Get tg user lang"""
     session = get_session()
     tg_user = session.query(TgUser).filter(TgUser.tg_id == tg_id).first()
-    return tg_user.lang if tg_user else None
+    return tg_user.lang if tg_user else tg_user
 
 
 def set_tg_user_lang(tg_id: int, lang: str):
     """Set tg user lang"""
     session = get_session()
-    tg_user = session.query(TgUser).filter(TgUser.tg_id == tg_id).first()
-    tg_user.lang = lang
+    session.query(TgUser).filter(TgUser.tg_id == tg_id).update({TgUser.lang: lang})
     session.commit()
 
 
-def get_active_tg_users() -> list[type[TgUser]]:
+def get_tg_users(active: bool, lang_code: str | None = None) -> list[type[TgUser]]:
     """Get active tg users"""
     session = get_session()
-    return session.query(TgUser).filter(TgUser.active == True).all()
+    tg_users = session.query(TgUser).filter(TgUser.active == active)
+    if lang_code:
+        tg_users = tg_users.filter(TgUser.lang == lang_code)
+    return tg_users.all()
 
 
 def set_tg_user_active(tg_id: int, active: bool):
     """Set tg user active"""
     session = get_session()
-    tg_user = session.query(TgUser).filter(TgUser.tg_id == tg_id).first()
-    tg_user.active = active
+    session.query(TgUser).filter(TgUser.tg_id == tg_id).update({TgUser.active: active})
     session.commit()
 
 
@@ -73,14 +79,5 @@ def increase_stats(stats_type: StatsType):
     """Increase stats"""
     session = get_session()
     stats = session.query(Stats).first()
-    if stats_type == StatsType.INLINE_SEARCHES:
-        stats.inline_searches += 1
-    elif stats_type == StatsType.MSG_SEARCHES:
-        stats.msg_searches += 1
-    elif stats_type == StatsType.BOOKS_READ:
-        stats.books_read += 1
-    elif stats_type == StatsType.PAGES_READ:
-        stats.pages_read += 1
-    elif stats_type == StatsType.JUMPS:
-        stats.jumps += 1
+    setattr(stats, stats_type.value, getattr(stats, stats_type.value) + 1)
     session.commit()
